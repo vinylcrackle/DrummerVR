afffe2c9f012bf317b724eede89f07737e1ab6f2
tarekuino <34999210+tarekuino@users.noreply.github.com>
2020-09-11 14:55:43 +0200

    Update and rename hatsanalyze.cs to HatsAnalyze.cs

--- Code/HatsAnalyze.cs
+++ Code/HatsAnalyze.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hatsanalyze : MonoBehaviour {
+public class HatsAnalyze : MonoBehaviour {
 
     public string txtFile = "hats";
     public double[] note_start; //Note Start Timings
